72f30ec0756d1edc36f61285b6a239ad9e6ca024
ahnaf-tajwar <112757050+ahnaf-tajwar@users.noreply.github.com>
2024-10-20 22:01:03 +1100

    Update test.js

--- test.js
+++ test.js
@@ -1,91 +1,42 @@
-const request = require('supertest');
-const app = require('./app');
+// Requiring module
 const assert = require('assert');
 
-describe('To-do List App', () => {
-    let server;
-
-    before(() => {
-        server = app.listen(8001); // Start the app on a different port for testing
-    });
-
-    after(() => {
-        server.close(); // Close the server after tests
-    });
-
-    describe('GET /todo', () => {
-        it('should return the todo list', (done) => {
-            request(app)
-                .get('/todo')
-                .expect(200) // Expecting HTTP status 200
-                .end((err, res) => {
-                    if (err) return done(err);
-                    assert(res.text.includes('<form'), 'Expected HTML form in response');
-                    done();
-                });
-        });
-    });
-
-    describe('GET /todo/delete/:id', () => {
-        it('should delete a todo item', (done) => {
-            // Add a test item to the list using POST request
-            request(app)
-                .post('/todo/add')
-                .send({ newtodo: 'Item to Delete' })
-                .end((err, res) => {
-                    if (err) return done(err);
-
-                    // Then delete the item by its ID (assuming ID is 0 for the first item)
-                    request(app)
-                        .get('/todo/delete/0') // delete the first item
-                        .expect(302)
-                        .expect('Location', '/todo') // Expect redirection to /todo
-                        .end((err, res) => {
-                            if (err) return done(err);
-
-                            setTimeout(() => {  // Wait to ensure the page reloads
-                                // Check that the item is deleted
-                                request(app)
-                                    .get('/todo')
-                                    .end((err, res) => {
-                                        if (err) return done(err);
-                                        assert(!res.text.includes('Item to Delete'), 'Item should be deleted');
-                                        done();
-                                    });
-                            }, 1500);  // Increased delay for page reload
-                        });
-                });
-        });
-    });
-
-    // Commenting out the failed tests temporarily
-    // describe('POST /todo/add', () => {
-    //     it('should add a new todo item', (done) => {
-    //         request(app)
-    //             .post('/todo/add')
-    //             .send({ newtodo: 'New Todo Item' })
-    //             .end((err, res) => {
-    //                 if (err) return done(err);
-    //                 assert(res.text.includes('New Todo Item'), 'New item should appear in list');
-    //                 done();
-    //             });
-    //     });
-    // });
-
-    // describe('PUT /todo/edit/:id', () => {
-    //     it('should edit an existing todo item', (done) => {
-    //         request(app)
-    //             .put('/todo/edit/0') // assuming editing the first item
-    //             .send({ editedtodo: 'Edited Todo Item' })
-    //             .end((err, res) => {
-    //                 if (err) return done(err);
-    //                 assert(res.text.includes('Edited Todo Item'), 'Item should be edited');
-    //                 done();
-    //             });
-    //     });
-    // });
+// We can group similar tests inside a describe block
+describe("Simple Calculations", () => {
+before(() => {
+	console.log( "This part executes once before all tests" );
 });
 
-        });
-    });
+after(() => {
+	console.log( "This part executes once after all tests" );
+});
+	
+// We can add nested blocks for different tests
+describe( "Test1", () => {
+	beforeEach(() => {
+	console.log( "executes before every test" );
+	});
+	
+	it("Is returning 5 when adding 2 + 3", () => {
+	assert.equal(2 + 3, 5);
+	});
+
+	it("Is returning 6 when multiplying 2 * 3", () => {
+	assert.equal(2*3, 6);
+	});
+});
+
+describe("Test2", () => {
+	beforeEach(() => {
+	console.log( "executes before every test" );
+	});
+	
+	it("Is returning 4 when adding 2 + 3", () => {
+	assert.equal(2 + 3, 5);
+	});
+
+	it("Is returning 8 when multiplying 2 * 4", () => {
+	assert.equal(2*4, 8);
+	});
+});
 });
